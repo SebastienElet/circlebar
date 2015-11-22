eb7323fb2097e224cbe9cf3d716a0ef509b38aee
Xavier Haniquaut <xavier.haniquaut@gmail.com>
2015-11-22 19:23:16 +0100

    Refacot Api Call
    Method get(endpoint, cb)

--- index.js
+++ index.js
@@ -28,56 +28,45 @@
   }
 
   function makeMenu() {
-    var endpoint = 'projects/';
-    var api = 'https://circleci.com/api/v1/';
-    var options = {
-      uri: api + endpoint,
-      qs: {
-        'circle-token': TOKEN,
-      },
-      headers: {
-        'User-Agent': 'Request-Promise',
-      },
-      json: true,
-    };
     var menu = [];
 
-    rp.get(options)
-      .then(function(values) {
-        var contextMenu;
+    get('projects/', function(values) {
+      var contextMenu;
 
-        menu.push({
-          label: BRANCH ? REPO + ':' + BRANCH : 'Pick a branch',
-          enabled: !!REPO,
-          click: function(menuItem) {
-            if (menuItem.enabled) {
-              shell.openExternal('https://circleci.com/gh/' + REPO + '/tree/' + BRANCH);
-            }
-          },
-        });
-        values.forEach(function(repo) {
-          var branches = Object.keys(repo.branches);
-          var branchesSubmenu = [];
-
-          branches.forEach(function(branchName) {
-            branchesSubmenu.push({
-              label: decodeURIComponent(branchName),
-              type: 'radio',
-              enabled: true,
-              click: function() {
-                REPO = 'SimpliField/' + repo.reponame;
-                BRANCH = branchName;
-                getCircleStatus();
-              },
-            });
-          });
-          menu.push({ label: repo.reponame, submenu: branchesSubmenu });
-        });
-        menu.push({ label: 'Quit', click: function() { app.quit(); } });
-
-        contextMenu = Menu.buildFromTemplate(menu);
-        mb.tray.setContextMenu(contextMenu);
+      menu.push({
+        label: BRANCH ? REPO + ':' + decodeURIComponent(BRANCH) : 'Pick a branch',
+        enabled: !!REPO,
+        click: function(menuItem) {
+          if (menuItem.enabled) {
+            shell.openExternal('https://circleci.com/gh/' + REPO + '/tree/' + BRANCH);
+          }
+        },
       });
+      menu.push({ type: 'separator' });
+      values.forEach(function(repo) {
+        var branches = Object.keys(repo.branches);
+        var branchesSubmenu = [];
+
+        branches.forEach(function(branchName) {
+          branchesSubmenu.push({
+            label: decodeURIComponent(branchName),
+            type: 'radio',
+            enabled: true,
+            click: function() {
+              REPO = 'SimpliField/' + repo.reponame;
+              BRANCH = branchName;
+              getCircleStatus();
+            },
+          });
+        });
+        menu.push({ label: repo.reponame, submenu: branchesSubmenu });
+      });
+      menu.push({ type: 'separator' });
+      menu.push({ label: 'Quit', click: function() { app.quit(); } });
+
+      contextMenu = Menu.buildFromTemplate(menu);
+      mb.tray.setContextMenu(contextMenu);
+    });
   }
 
   function getCircleStatus() {
@@ -86,17 +75,6 @@
     printStatus(endpoint);
   }
   function printStatus(endpoint) {
-    var api = 'https://circleci.com/api/v1/';
-    var options = {
-      uri: api + endpoint,
-      qs: {
-        'circle-token': TOKEN,
-      },
-      headers: {
-        'User-Agent': 'Request-Promise',
-      },
-      json: true,
-    };
     var icons = {
       cancelled: 'images/icon_cancelled.png',
       failed: 'images/icon_failed.png',
@@ -106,16 +84,32 @@
     };
 
     if ('' !== BRANCH) {
-      rp.get(options)
-        .then(function getLast(builds) {
-          mb.tray.setToolTip(builds[0].reponame + ': ' + builds[0].branch);
-          mb.tray.setImage(path.join(__dirname, icons[builds[0].status || 'default']));
-          makeMenu();
-        })
-        .catch(function error(err) {
-          console.log('API call failed...', err);
-        });
+      get(endpoint, function getLast(builds) {
+        mb.tray.setToolTip(builds[0].reponame + ': ' + builds[0].branch);
+        mb.tray.setImage(path.join(__dirname, icons[builds[0].status || 'default']));
+        makeMenu();
+      })
     }
+  }
+
+  function get(endpoint, callback) {
+    var api = 'https://circleci.com/api/v1/';
+    var options = {
+      uri: api + endpoint,
+      qs: {
+        'circle-token': TOKEN,
+      },
+      headers: {
+        'User-Agent': 'Request-Promise',
+      },
+      json: true,
+    };
+
+    rp.get(options)
+      .then(callback)
+      .catch(function error(err) {
+        console.log('API call failed...', err);
+      })
   }
 
 }());
